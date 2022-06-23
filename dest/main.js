f6439ba3691819cf2300224c8cb14015aa62f5a7
thai <hongthai2007.hongthai2007@gmail.com>
2022-06-23 15:52:55 +0700

    2nd GBOX Done Styling for Homepage -not yet JS and Resposive

--- dest/main.js
+++ dest/main.js
@@ -1,1 +1,25 @@
-console.log('Hello CFD Circle');
+
+//Gallery
+
+let listGallery = document.querySelectorAll(".product .product__item")
+let btnGallery = document.querySelectorAll(".product .product__menu a")
+let currentBtn = 0;
+
+btnGallery.forEach(function(element,index){
+    if(element.classList.contains("active")){
+        currentBtn = index;
+    }
+
+    element.addEventListener("click",function(event){
+        event.preventDefault();
+
+        btnGallery[currentBtn].classList.remove("active")
+        listGallery[currentBtn].classList.remove("active")
+
+        btnGallery[index].classList.add("active")
+        listGallery[index].classList.add("active")
+
+        currentBtn = index
+    })
+})
+
